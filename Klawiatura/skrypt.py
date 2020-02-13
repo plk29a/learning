8768e7696235ba2a68e86d4d3c20aa40df21e199
plk29a <plk29a@gmail.com>
2020-02-13 22:06:36 +0100

    program do symulowania klawiatury

---
+++ Klawiatura/skrypt.py
@@ -0,0 +1,15 @@
+import time
+import keyboard
+
+
+#help(keyboard)
+
+time.sleep(2)
+print('ok')
+
+keyboard.press_and_release('alt, right, down')
+
+print('done')
+
+time.sleep(5)
+print('bye bye')
